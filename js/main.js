6851b6d16d8681b17be9c7f9c9cae5eb4263e059
fofoy <florian.quiblier@gmail.com>
2012-11-29 16:04:07 +0100

    Création du header du site

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	/*$(window).scroll(function () { 
+      console.log('ok');
+	});*/
+	/*$("#content").mousewheel(function(event, delta) {
+	  this.scrollLeft -= (delta * 30);
+	  event.preventDefault();
+	});*/
+});
