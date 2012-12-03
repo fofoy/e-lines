d39b8c3cf561c04ade386f1e119ab5c8834ea3ad
Maxime Debavelaere <maxime.debavelaere@gmail.com>
2012-12-03 22:12:39 +0100

    Modif main.js scrollbar

--- js/main.js
+++ js/main.js
@@ -1,32 +1,4 @@
 $(function(){
-
-	// Bind scrollable content
-	$('#scrollbar1').tinyscrollbar({ axis: 'x', invertscroll: true });
-
-    var scrollbar1 = $('#scrollbar1');
-	var wcategory = $(".category").width();
-    //add a click event to a button
-
-    $('#scrollbar1-anchor').click(function(){
-        scrollbar1.tinyscrollbar_update(0);
-        return false;
-    });
-    $('#scrollbar2-anchor').click(function(){
-        scrollbar1.tinyscrollbar_update(wcategory);
-        return false;
-    });
-    $('#scrollbar3-anchor').click(function(){
-        scrollbar1.tinyscrollbar_update(wcategory*2);
-        return false;
-    });
-    $('#scrollbar4-anchor').click(function(){
-        scrollbar1.tinyscrollbar_update(wcategory*3);
-        return false;
-    });
-    $('#scrollbar5-anchor').click(function(){
-        scrollbar1.tinyscrollbar_update(wcategory*4);
-        return false;
-    });
     
 	// Bind tooltips
 	$('nav ul li a').tipsy({gravity:'s'});
@@ -73,4 +45,34 @@ $(function(){
         $('.category').width(category);
         $('.overview').width(overview);
     }
+
+    //Scrollbar
+    
+    // Bind scrollable content
+    $('#scrollbar1').tinyscrollbar({ axis: 'x', invertscroll: true });
+
+    var scrollbar1 = $('#scrollbar1');
+    var wcategory = $(".category").width();
+    //add a click event to a button
+
+    $('#scrollbar1-anchor').click(function(){
+        scrollbar1.tinyscrollbar_update(0);
+        return false;
+    });
+    $('#scrollbar2-anchor').click(function(){
+        scrollbar1.tinyscrollbar_update(wcategory);
+        return false;
+    });
+    $('#scrollbar3-anchor').click(function(){
+        scrollbar1.tinyscrollbar_update(wcategory*2);
+        return false;
+    });
+    $('#scrollbar4-anchor').click(function(){
+        scrollbar1.tinyscrollbar_update(wcategory*3);
+        return false;
+    });
+    $('#scrollbar5-anchor').click(function(){
+        scrollbar1.tinyscrollbar_update(wcategory*4);
+        return false;
+    });
 });
