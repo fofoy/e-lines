9d335fcb58c23794e8b04b07a1e078904aa1993c
Maxime Debavelaere <maxime.debavelaere@gmail.com>
2012-12-05 02:12:57 +0100

    Video single correct id

--- js/vimeo.js
+++ js/vimeo.js
@@ -1,44 +1,37 @@
 
-// Change this to your username to load in your clips
-var vimeoUserName = 'chamonix';
+function getID(){
+        var regexS = "[\\?&]id=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(window.location.search);
+        if(results == null){
+            return "";
+        }
+        else{
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+    }
 
-// Tell Vimeo what function to call
-var videoCallback = 'latestVideo';
-var oEmbedCallback = 'embedVideo';
+var id_video = getID();
+var apiEndpoint = 'http://vimeo.com/api/v2/';
+var oEmbedEndpoint = 'http://vimeo.com/api/oembed.json'
+var oEmbedCallback = 'switchVideo';
+var videosCallback = 'setupGallery';
+var vimeoUsername = 'brad';
 
-// Set up the URLs
-var videosUrl = 'http://vimeo.com/api/v2/' + vimeoUserName + '/videos.json?callback=' + videoCallback;
-var oEmbedUrl = 'http://vimeo.com/api/oembed.json';
+$(document).ready(function() {
+            $.getScript(apiEndpoint + vimeoUsername + '/videos.json?callback=' + videosCallback);
+        });
 
-// This function puts the video on the page
-function embedVideo(video) {
-    videoEmbedCode = video.html;
-    document.getElementById('embed').innerHTML = unescape(video.html);
-}
+        function getVideo(url) {
+            $.getScript(oEmbedEndpoint + '?url=http://vimeo.com/' + id_video + '&width=504&height=280&callback=' + oEmbedCallback);
+        }
 
-// This function uses oEmbed to get the last clip
-function latestVideo(videos) {
-    var videoUrl = videos[0].url;
-    document.getElementById('title_video_big').innerHTML = videos[0].title;
+        function setupGallery(videos) {
+            // Load the first video
+            getVideo(videos[0].url);
+        }
 
-    // Get the oEmbed stuff
-    loadScript(oEmbedUrl + '?url=' + encodeURIComponent(videoUrl) + '&callback=' + oEmbedCallback);
+        function switchVideo(video) {
+            $('#embed').html(unescape(video.html));
+        }
 
-    for (var i = 1; i < 5; i++) {
-      document.getElementById('title_video_small'+(i-1)).innerHTML = videos[i].title;
-      document.getElementById('thumb_video_small'+(i-1)).setAttribute('src', videos[i].thumbnail_large);
-    };
-}
-
-// This function loads the data from Vimeo
-function loadScript(url) {
-    var js = document.createElement('script');
-    js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', url);
-    document.getElementsByTagName('head').item(0).appendChild(js);
-}
-
-// Call our init function when the page loads
-window.onload = function() {
-    loadScript(videosUrl);
-};
