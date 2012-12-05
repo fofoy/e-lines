714a13c609e197126ce3359146d1025d072a8cd0
Thomas Senkman <thomassenkman@me.com>
2012-12-06 00:44:53 +0100

    Génération videos featured hub

--- js/vimeo_hub.js
+++ js/vimeo_hub.js
@@ -11,11 +11,21 @@ var videosUrl = 'http://vimeo.com/api/v2/album/' + vimeoAlbum + '/videos.json?ca
 // This function uses oEmbed to get the last clip
 function latestVideo(videos) {
     for (var i = 0; i < 7; i++) {
+      //generation des vidéos dans "videos"
       document.getElementById('title_video_small'+(i)).innerHTML = videos[i].title;
       document.getElementById('link_video_small'+(i)).setAttribute('id', videos[i].id);
       document.getElementById('thumb_video_small'+(i)).setAttribute('src', videos[i].thumbnail_large);
       $("#main div.category:nth(1) ul li:nth("+i+") a").attr("href", "video.html?id="+videos[i].id);//regénération des url avec les id vimeo
       $("#main div.category:nth(1) ul li:nth("+i+") a img").attr("alt", videos[i].title);//alt
+
+      //generation des vidéos dans "featured"
+      if(i==1||i==5){
+      document.getElementById('title_video_small_f'+(i)).innerHTML = videos[i].title;
+      document.getElementById('link_video_small_f'+(i)).setAttribute('id', videos[i].id);
+      document.getElementById('thumb_video_small_f'+(i)).setAttribute('src', videos[i].thumbnail_large);
+      $("#main div.category:nth(0) ul li:nth("+i+") a").attr("href", "video.html?id="+videos[i].id);//regénération des url avec les id vimeo
+      $("#main div.category:nth(0) ul li:nth("+i+") a img").attr("alt", videos[i].title);//alt
+      }
 
       //Mise à jour dynamique du JSON pour la fonction de recherche
       donnees.item[7+i].id=videos[i].id; 
