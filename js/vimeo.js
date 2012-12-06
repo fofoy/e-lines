8f31ceaf9e7f471739e2eb2eb1d11733c838b549
Thomas Senkman <thomassenkman@me.com>
2012-12-06 22:46:31 +0100

    url des videos video.html

--- js/vimeo.js
+++ js/vimeo.js
@@ -50,6 +50,10 @@ $(document).ready(function() {
                 div.setAttribute('class', 'video');
                 div.innerHTML = videos[j].title;
                 a.appendChild(div);
+
+                //Reecriture des url des vidéos liées
+                $("#liste_video li:nth("+j+") a").attr("href", "video.html?id="+videos[j].id);//regénération des url avec les id vimeo
+                $("#liste_video li:nth("+j+") a img").attr("alt", videos[j].title);//alt
             }
 
             // Switch to the video when a thumbnail is clicked
