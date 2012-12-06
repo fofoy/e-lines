4576bed1bdc90ea2f8248005686d8f49107a833e
Thomas Senkman <thomassenkman@me.com>
2012-12-07 00:01:12 +0100

    Search fix doublon

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ $(function(){
             $field = $("#q");
             $.each(donnees.item, function() {
                 if(this.titre.toLowerCase().indexOf($field.val().toLowerCase())!=-1||this.soustitre.toLowerCase().indexOf($field.val().toLowerCase())!=-1){
-                    if($("#s_"+this.type).hasClass('selected')){
+                    if($("#s_"+this.type).hasClass('selected')&&this.featured=="no"){
                         $("#res_search").append("<li><a href='"+this.type+".html?id="+this.id+"'><span class='"+this.type+"'></span>"+this.titre+"</li>");
                         hasRes = true;
                     }
