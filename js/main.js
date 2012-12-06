572c97f24fc59362476fce38b8428ea7f3fa4169
fofoy <florian.quiblier@gmail.com>
2012-12-06 21:53:09 +0100

    Fix page video

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ $(function(){
         $('.category').width(category);
         $('.overview').width(overview);
         if ($('.big_video').length > 0){
-            $('.big_video').height(content - marginBlocks*2);
+            $('.big_video').height(content - marginBlocks*2 - h2Height);
             $('.big_video').width($('.big_video').height() * 16/9 ); /* Temp */
             $('.video_content .small').height((content - marginBlocks*17)/4); /* Temp */
             $('.overview').width($('.big_video').width() + blockSmallWidth + marginContent); /* Temp */
@@ -106,7 +106,7 @@ $(function(){
             $.each(donnees.item, function() {
                 if(this.titre.toLowerCase().indexOf($field.val().toLowerCase())!=-1||this.soustitre.toLowerCase().indexOf($field.val().toLowerCase())!=-1){
                     if($("#s_"+this.type).hasClass('selected')){
-                        $("#res_search").append("<li><a href='"+this.type+".html?id="+this.id+"'><span class='"+this.type+"'></span>"+this.titre+"</li>")
+                        $("#res_search").append("<li><a href='"+this.type+".html?id="+this.id+"'><span class='"+this.type+"'></span>"+this.titre+"</li>");
                         hasRes = true;
                     }
                 }
