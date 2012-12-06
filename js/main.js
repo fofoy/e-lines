d822eb5b14a6f5fd36db7b6b7c2b3017476186c7
Aurélien Dias <aurelien.esiao@gmail.com>
2012-12-07 00:11:52 +0100

    cleaning js a bit

--- js/main.js
+++ js/main.js
@@ -119,23 +119,7 @@ $(function(){
             event.preventDefault();
         });
 
-    // ----------------------------------------------------------
-    // A short snippet for detecting versions of IE in JavaScript
-    // without resorting to user-agent sniffing
-    // ----------------------------------------------------------
-    // If you're not in IE (or IE version is less than 5) then:
-    //     ie === undefined
-    // If you're in IE (>=5) then you can determine which version:
-    //     ie === 7; // IE7
-    // Thus, to detect IE:
-    //     if (ie) {}
-    // And to detect the version:
-    //     ie === 6 // IE6
-    //     ie > 7 // IE8, IE9 ...
-    //     ie < 9 // Anything less than IE9
-    // ----------------------------------------------------------
-     
-    // UPDATE: Now using Live NodeList idea from @jdalton
+    // Detect IE
      
     var ie = (function(){
      
@@ -152,7 +136,7 @@ $(function(){
      return v > 4 ? v : undef;
      
     }());
-    console.log(ie);
+
     if (ie <= 9) {
         $('#choice ul li a').css({ opacity: 1 });
     }
