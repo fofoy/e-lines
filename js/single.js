814340f93d92cba51df4a5007e52f12078f773bf
Thomas Senkman <thomassenkman@me.com>
2012-12-07 04:21:27 +0100

    Reprise athletes + event sur la base d'aurélien

--- js/single.js
+++ js/single.js
@@ -1,13 +1,17 @@
 //Generation des donnees des pages single depuis le JSON
 $(document).ready( function() {
     var id_i = getID();
-    var cpt = 0;
     $.each(donnees.item, function() {
         if(this.id==id_i&&this.featured=="no"){
-            $("#title-single").append("<a href='#'>"+this.titre+" - "+this.soustitre+"</a>");
-            $("#content").append('<img src="'+this.image+'" alt="'+this.titre+'" />');
-            $("#content").append('<p id="description">'+this.description+'</p>');
+            $("#title-single").append(this.titre+" - "+this.soustitre);
+            $("#biopic img").attr("src", this.image);
+            $("#biopic img").attr("alt", this.titre);
+            if(this.type=="athlete"){
+                $("#description").append('<h3>BIOGRAPHY</h3>');
+            }else{
+                $("#description").append('<h3>ABOUT</h3>');
+            }
+            $("#description").append('<p>'+this.description+'</p><p>'+this.description+'</p>');
         }
-        cpt++;
     });
 });
