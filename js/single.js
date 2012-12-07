a28158a8bd87fc6cb98d1d2979b5d69e6758e4bf
Thomas Senkman <thomassenkman@me.com>
2012-12-07 03:53:11 +0100

    Singles

--- js/single.js
+++ js/single.js
@@ -1,11 +1,12 @@
+//Generation des donnees des pages single depuis le JSON
 $(document).ready( function() {
     var id_i = getID();
     var cpt = 0;
     $.each(donnees.item, function() {
         if(this.id==id_i&&this.featured=="no"){
-            $("#title-single").append(this.titre+" - "+this.soustitre);
+            $("#title-single").append("<a href='#'>"+this.titre+" - "+this.soustitre+"</a>");
             $("#content").append('<img src="'+this.image+'" alt="'+this.titre+'" />');
-            $("#content").append('<p>'+this.description+'</p>');
+            $("#content").append('<p id="description">'+this.description+'</p>');
         }
         cpt++;
     });
